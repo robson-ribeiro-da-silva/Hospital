2d21db1aeaaf65995d000d0330dd0687ce0956e4
Deidson Lisboa <deidson39@hotmail.com>
2018-07-05 20:07:17 -0300

    correção

--- app/models/Medicamento.java
+++ app/models/Medicamento.java
@@ -9,22 +9,5 @@ public class Medicamento extends Model {
 	
 	public String nomeMedicamento;
 	public String fabricante;
-	public Medicamento(String nomeMedicamento, String fabricante) {
-		super();
-		this.nomeMedicamento = nomeMedicamento;
-		this.fabricante = fabricante;
-	}
-	public String getNomeMedicamento() {
-		return nomeMedicamento;
-	}
-	public void setNomeMedicamento(String nomeMedicamento) {
-		this.nomeMedicamento = nomeMedicamento;
-	}
-	public String getFabricante() {
-		return fabricante;
-	}
-	public void setFabricante(String fabricante) {
-		this.fabricante = fabricante;
-	}
-
+	
 }
